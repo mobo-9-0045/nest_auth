1d40d51263bc07b35139f0089411b3e3d9379b7b
DST <abdelilah@gmail.com>
2024-08-26 14:12:13 -0500

    added authentication guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,36 @@
+import { ExecutionContext, CanActivate, Injectable, UnauthorizedException} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Http2ServerRequest } from "http2";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+    constructor(private jawtService: JwtService){}
+
+    async canActivate(context: ExecutionContext): Promise<boolean>{
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token){
+            throw new UnauthorizedException();
+        }
+        try{
+            const jwtSecKey: string = "JWT SUPER SECRET";
+            const payload = await this.jawtService.verify(
+                token,
+                {
+                    secret: jwtSecKey,
+                }
+            );
+            request['user'] = payload;
+        }
+        catch{
+            throw new UnauthorizedException();
+        }
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Http2ServerRequest): string | undefined{
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
+}
